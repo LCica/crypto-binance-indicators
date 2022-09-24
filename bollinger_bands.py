6d5380904151d8cdc163a64b8579d98d232c66e4
Luka Cica <luka.cica.2k1@gmail.com>
2022-09-24 16:15:22 +0200

    Importing Getdata

--- bollinger_bands.py
+++ bollinger_bands.py
@@ -1,17 +1,7 @@
 from binance import Client
 import pandas as pd 
+from getdata import gethourdata
 
-client = Client("TYPE YOUR API KEY HERE","TYPE YOUR SECRET KEY HERE")
-
-#function to get market data for a coin(symbol) and for interval(ex. 1h) for lookback length.
-def gethourdata(symbol, interval, lookback):
-    frame = pd.DataFrame(client.get_historical_klines(symbol,interval,lookback + 'hours ago UTC'))
-    frame=frame.iloc[:,:6]
-    frame.columns=['Time','Open','High','Low','Close','Volume']
-    frame=frame.set_index('Time')
-    frame.index=pd.to_datetime(frame.index, unit='ms')
-    frame= frame.astype(float)
-    return frame
 
 def sma(data, window):
     return(data.rolling(window = window).mean())
