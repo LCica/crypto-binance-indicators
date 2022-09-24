f3d4152f57f19335a22a5fde20bad2acd1efcfff
Luka Cica <luka.cica.2k1@gmail.com>
2022-09-24 16:09:39 +0200

    Saving GetData In Seperate File

---
+++ getdata.py
@@ -0,0 +1,13 @@
+from binance import Client
+import pandas as pd
+
+
+def gethourdata(symbol, interval, lookback):
+    client = Client("TYPE YOUR API KEY HERE","TYPE YOUR SECRET KEY HERE")
+    frame = pd.DataFrame(client.get_historical_klines(symbol,interval,lookback + 'hours ago UTC'))
+    frame=frame.iloc[:,:6]
+    frame.columns=['Time','Open','High','Low','Close','Volume']
+    frame=frame.set_index('Time')
+    frame.index=pd.to_datetime(frame.index, unit='ms')
+    frame= frame.astype(float)
+    return frame
