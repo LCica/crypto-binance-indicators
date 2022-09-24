8ef50383e9c226da752b1218ff15ad6794fe570d
Luka Cica <luka.cica.2k1@gmail.com>
2022-09-24 16:16:56 +0200

    Importing Getdata

--- macd.py
+++ macd.py
@@ -1,17 +1,6 @@
-from binance import Client
 import pandas as pd 
 import ta
-client = Client("TYPE YOUR API KEY HERE","TYPE YOUR SECRET KEY HERE")
-
-#function to get market data for a coin(symbol) and for interval(ex. 1h) for lookback length.
-def gethourdata(symbol, interval, lookback):
-    frame = pd.DataFrame(client.get_historical_klines(symbol,interval,lookback + 'hours ago UTC'))
-    frame=frame.iloc[:,:6]
-    frame.columns=['Time','Open','High','Low','Close','Volume']
-    frame=frame.set_index('Time')
-    frame.index=pd.to_datetime(frame.index, unit='ms')
-    frame= frame.astype(float)
-    return frame
+from getdata import gethourdata
 
 def macd(symbol,interval,lookback):
     df = gethourdata(symbol, interval, lookback)
