87cadf230267d91893c7d0eeb048ace9371262a2
Luka Cica <luka.cica.2k1@gmail.com>
2022-09-29 19:40:23 +0200

    Added On Balance Volume Indicator

---
+++ indicators/obv.py
@@ -0,0 +1,13 @@
+from getdata import gethourdata
+
+def obv(symbol,interval,lookback):
+    df=gethourdata(symbol,interval,lookback)
+    k=float(df['Volume'][0])
+    for i in range(1,len(df['Volume'])):
+        if df['Close'][i]>df['Close'][i-1]:
+            k+=float(df['Volume'][i])
+        elif df['Close'][i]<df['Close'][i-1]:
+            k-=float(df['Volume'][i])
+    return k
+
+#Example Of Running The Code:obv('BTCUSDT','1h','1000')
