2acfd7efc316d3790db0580814af8e972eac9e0e
Luka Cica <luka.cica.2k1@gmail.com>
2022-10-09 17:07:42 +0200

    Added One More Message For The Bot

--- helpful_functions/gmail_messages.py
+++ helpful_functions/gmail_messages.py
@@ -1,9 +1,9 @@
 import smtplib, ssl
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "type in sender_email"  
-receiver_email = "type in receiver_email"  
-password = "type in password"
+sender_email = "kojikurac2k1@gmail.com"  
+receiver_email = "kojikurac2k1+binance@gmail.com"  
+password = "fitlrgcrbmlrkpxu"
 context = ssl.create_default_context()
 
 def sell_message(price):
@@ -20,5 +20,15 @@ def buy_message(price):
         Subject: Binance message
 
         You bought for: {} EUR""".format(price)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message2)
+def percentage(proc,buy,sell):
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:    
+        message2 = """\
+        Subject: Binance message
+
+        Your buy was at {} EUR,
+        Your sell was at {} EUR,
+        Your percentage profit/loss is: {} %""".format(buy,sell,proc)
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message2)
